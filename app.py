3be29849461f2eb9b1f3e4ea133996bbda299ab9
chantiredd <159920883+chantiredd@users.noreply.github.com>
2024-04-23 14:13:13 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+# app.py
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('index2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
